86af15970c1af1df5f49d0cf362956099f596529
Silpa <rkmsilpa29@gmail.com>
2022-04-07 20:55:57 +0100

    Final changes_assignment evening

--- specs/partC.js
+++ specs/partC.js
@@ -1,20 +1,21 @@
 import{assert} from 'chai'
 describe('Scenario_1_Part C assignment',()=>{
+    let name = ["Jackson","Rob","Jack","Wilson","John"]
     it('Test001_Length of the list',()=>{
-        let name = ["Jackson","Rob","Jack","Wilson","John"]
+        
         let len = name.length
         assert.equal(len,5,'Test case 001 pass')
     })
 
     it('Test002_Daniel in the list',()=>{
-        let name = ["Jackson","Rob","Jack","Wilson","John"]
+        //let name = ["Jackson","Rob","Jack","Wilson","John"]
         
         assert.include(name,'Daniel','contains')
     })
     
     it('Test003_Filter in the list',()=>{
-        let name = ["Jackson","Rob","Jack","Wilson","John"]
-        let newName = name.filter((fil)=> fil.includes('Jack'))
+       // let name = ["Jackson","Rob","Jack","Wilson","John"]
+        let newName = name.filter((fil)=> fil.startsWith('Jack'))
         let count = newName.length
         console.log(newName)
        assert.equal(count,2)
